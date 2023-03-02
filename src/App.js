9f0cf0459ed23877b5d4eb72a8fe2753bd43ad22
MLAngelo0812 <leo812panda@outlook.com>
2023-03-02 04:10:11 -0600

    myportfolio

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Container from 'react-bootstrap/Container';
+// import { Row, Col } from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>
+          <a href="/">Home</a>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+export default App;
